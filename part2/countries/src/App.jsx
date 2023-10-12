5b9376cb9fb61ddd418f3657744a181e1d174eb9
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-12 23:04:07 +0300

    2.18 done

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
       .then((data) => setCountries(data));
   }, []);
 
-  const matches = countries.filter((c) => c.name.common.includes(filter));
+  const matches = countries.filter((c) =>
+    c.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <>
@@ -22,7 +24,22 @@ function App() {
       ) : matches.length > 1 ? (
         matches.map((m, i) => <div key={i}>{m.name.common}</div>)
       ) : (
-        <></>
+        matches.length === 1 && (
+          <>
+            <h1>{matches[0].name.common}</h1>
+            <p>capital {matches[0].capital}</p>
+            <p>area {matches[0].area}</p>
+            <p>
+              <b>languages:</b>
+            </p>
+            <ul>
+              {Object.entries(matches[0].languages).map(([id, lang]) => (
+                <li key={id}>{lang}</li>
+              ))}
+            </ul>
+            <img src={matches[0].flags.png} />
+          </>
+        )
       )}
     </>
   );
