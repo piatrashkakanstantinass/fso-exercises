c36244ba9afa7fb53d5e21de0ee9413ba891a0e9
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-12 23:44:13 +0300

    Complete part2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState("");
+  const [temp, setTemp] = useState(null);
+  const [windSpeed, setWindSpeed] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState(null);
 
   useEffect(() => {
     fetch("https://studies.cs.helsinki.fi/restcountries/api/all")
@@ -14,19 +17,51 @@ function App() {
     c.name.common.toLowerCase().includes(filter.toLowerCase())
   );
 
+  let [lat, lon] = [null, null];
+  if (matches.length === 1) {
+    [lat, lon] = matches[0].latlng;
+  }
+
+  useEffect(() => {
+    let ignore = false;
+    if (lat !== null && lon !== null) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (!ignore) {
+            setTemp(data.main.temp);
+            setWindSpeed(data.wind.speed);
+            setWeatherIcon(data.weather[0].icon);
+          }
+        });
+    }
+
+    return () => (ignore = true);
+  }, [lat, lon]);
+
   return (
     <>
       <div>
-        find countries <input onChange={(e) => setFilter(e.target.value)} />
+        find countries{" "}
+        <input value={filter} onChange={(e) => setFilter(e.target.value)} />
       </div>
       {matches.length > 10 ? (
         <div>Too many matches, specify another filter</div>
       ) : matches.length > 1 ? (
-        matches.map((m, i) => <div key={i}>{m.name.common}</div>)
+        matches.map((m, i) => (
+          <div key={i}>
+            {m.name.common}{" "}
+            <button onClick={() => setFilter(m.name.common)}>show</button>
+          </div>
+        ))
       ) : (
         matches.length === 1 && (
           <>
-            <h1>{matches[0].name.common}</h1>
+            <h2>{matches[0].name.common}</h2>
             <p>capital {matches[0].capital}</p>
             <p>area {matches[0].area}</p>
             <p>
@@ -38,6 +73,16 @@ function App() {
               ))}
             </ul>
             <img src={matches[0].flags.png} />
+            {temp !== null && (
+              <>
+                <h2>Weather in {matches[0].capital}</h2>
+                <p>temperature {temp} Celcius</p>
+                <img
+                  src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+                />
+                <p>wind {windSpeed} m/s</p>
+              </>
+            )}
           </>
         )
       )}
