fef07fa8e8d0a9b9b2b85895733305a7d2c87176
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-28 18:01:40 +0300

    extract model

--- part4/bloglist/server.js
+++ part4/bloglist/server.js
@@ -2,16 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 require("dotenv").config();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 mongoose.connect(process.env.MONGODB_URI);
 
