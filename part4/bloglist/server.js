488e21894760e1f62c03faa80a3d2fd8405e74f3
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-28 18:19:53 +0300

    extract logger

--- part4/bloglist/server.js
+++ part4/bloglist/server.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 mongoose.connect(config.MONGODB_URI);
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
