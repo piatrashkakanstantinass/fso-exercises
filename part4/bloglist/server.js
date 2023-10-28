4ba910dce7e744cb6739a6b3b9f58a18174d4a17
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-28 18:07:51 +0300

    extract controller

--- part4/bloglist/server.js
+++ part4/bloglist/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const notesRouter = require("./controllers/blogs");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -10,19 +10,7 @@ mongoose.connect(process.env.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", notesRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
