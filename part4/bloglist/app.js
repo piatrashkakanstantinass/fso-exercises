cf65672b5c11dd140683e0e46490aff32d33aa76
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-28 18:21:41 +0300

    move db connection to app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const notesRouter = require("./controllers/blogs");
+const config = require("./utils/config");
 
 const app = express();
+
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
--- part4/bloglist/server.js
+++ part4/bloglist/server.js
@@ -1,9 +1,6 @@
-const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-mongoose.connect(config.MONGODB_URI);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
