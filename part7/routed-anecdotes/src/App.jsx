e7b6cad15d239a1d9d5868c757db302dd329f3ec
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-11-10 21:09:17 +0200

    routed-anecdotes: use input field hook

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -7,6 +7,7 @@ import {
   useNavigate,
   Navigate,
 } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -86,16 +87,16 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const [content, resetContent] = useField("text");
+  const [author, resetAuthor] = useField("text");
+  const [info, resetInfo] = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
   };
@@ -106,29 +107,27 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button
+          type="button"
+          onClick={() => {
+            resetContent();
+            resetAuthor();
+            resetInfo();
+          }}
+        >
+          reset
+        </button>
       </form>
     </div>
   );
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return [
+    {
+      type,
+      value,
+      onChange,
+    },
+    () => setValue(""),
+  ];
+};
+
+// modules can have several named exports
+
+export const useAnotherHook = () => {
+  // ...
+};
